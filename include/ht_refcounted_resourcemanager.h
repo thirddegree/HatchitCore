59834ee3d537ceda2e9d84a2dce6c2d3b54b34d1
Kyle Larson <klarson1331@gmail.com>
2016-04-07 19:32:25 -0400

    RefCounted classes may now forward arguments to their VarType constructors.

--- include/ht_refcounted_resourcemanager.h
+++ include/ht_refcounted_resourcemanager.h
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <string>
+#include <assert.h>
 #include <ht_platform.h>
 #include <ht_singleton.h>
 
@@ -12,8 +13,12 @@ namespace Hatchit
         class HT_API RefCountedResourceManager : public Singleton<RefCountedResourceManager>
         {
         public:
-            template<typename ResourceType>
-            static ResourceType* GetRawPointer(std::string name);
+            ~RefCountedResourceManager()
+            {
+                assert(m_resources.size() == 0);
+            }
+            template<typename ResourceType, typename... Args>
+            static ResourceType* GetRawPointer(std::string name, Args&&... arguments);
 
             template<typename ResourceType>
             static void ReleaseRawPointer(const std::string& name);
@@ -24,26 +29,26 @@ namespace Hatchit
             std::map<std::string, void*> m_resources;
         };
 
-        template<typename ResourceType>
-        ResourceType* RefCountedResourceManager::GetRawPointer(std::string name)
+        template<typename ResourceType, typename... Args>
+        ResourceType* RefCountedResourceManager::GetRawPointer(std::string name, Args&&... arguments)
         {
             if (name.empty())
                 return nullptr;
 
             //Create a typed name so two different types can use the same name
-            std::string typedName = name + typeid(ResourceType).raw_name();
+            name += std::to_string(typeid(ResourceType).hash_code());
 
             RefCountedResourceManager& _instance = RefCountedResourceManager::GetInstance();
 
-            std::map<std::string, void*>::iterator it = _instance.m_resources.find(typedName);
+            std::map<std::string, void*>::iterator it = _instance.m_resources.find(name);
             if (it == _instance.m_resources.end())
             {
                 //resource not found.  Must allocate
-                ResourceType* resource = new ResourceType(name); //Give the untyped name to the constructor so we can work with the original filename
-                _instance.m_resources.insert(std::make_pair(typedName, resource));
+                ResourceType* resource = new ResourceType(name, std::forward<Args>(arguments)...);
+                _instance.m_resources.insert(std::make_pair(name, resource));
             }
 
-            return reinterpret_cast<ResourceType*>(_instance.m_resources[typedName]);
+            return reinterpret_cast<ResourceType*>(_instance.m_resources[name]);
         }
 
         template<typename ResourceType>
@@ -51,13 +56,10 @@ namespace Hatchit
         {
             RefCountedResourceManager& _instance = RefCountedResourceManager::GetInstance();
 
-            //Create a typed name so two different types can use the same name
-            std::string typedName = name + typeid(ResourceType).raw_name();
-
-            std::map<std::string, void*>::iterator it = _instance.m_resources.find(typedName);
+            std::map<std::string, void*>::iterator it = _instance.m_resources.find(name);
             if (it != _instance.m_resources.end())
             {
-                delete (reinterpret_cast<ResourceType*>(_instance.m_resources[typedName]));
+                delete (reinterpret_cast<ResourceType*>(_instance.m_resources[name]));
 
                 _instance.m_resources.erase(it);
             }
