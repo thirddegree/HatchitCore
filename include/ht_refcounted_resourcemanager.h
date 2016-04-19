b4fb55e604b49fba3f1a61ba53b01098671f7ffe
Kyle Larson <klarson1331@gmail.com>
2016-04-19 13:30:08 -0400

    Changes to refCountedResourceManager to use Guids instead of strings for keys.  NOT STABLE

--- include/ht_refcounted_resourcemanager.h
+++ include/ht_refcounted_resourcemanager.h
@@ -14,12 +14,14 @@
 
 #pragma once
 
-#include <map>
-#include <string>
-#include <typeinfo>
-#include <ht_platform.h>
-#include <ht_singleton.h>
-#include <cassert>
+//Header Includes
+#include <ht_platform.h> //HT_API
+#include <ht_singleton.h> //Singleton<T>
+#include <map> //std::map<T, U>
+
+//Inline includes
+#include <ht_guid.h> //Guid
+#include <cassert> //assert()
 
 namespace Hatchit
 {
@@ -28,61 +30,21 @@ namespace Hatchit
         class HT_API RefCountedResourceManager : public Singleton<RefCountedResourceManager>
         {
         public:
-            ~RefCountedResourceManager()
-            {
-                assert(m_resources.size() == 0);
-            }
+            RefCountedResourceManager();
+            ~RefCountedResourceManager();
+            
             template<typename ResourceType, typename... Args>
-            static ResourceType* GetRawPointer(std::string name, Args&&... arguments);
+            static ResourceType* GetRawPointer(const Guid& ID, Args&&... arguments);
 
             template<typename ResourceType>
-            static void ReleaseRawPointer(const std::string& name);
+            static void ReleaseRawPointer(const Guid& name);
 
         private:
             static RefCountedResourceManager& GetInstance();
 
-            std::map<std::string, void*> m_resources;
+            std::map<Guid, void*> m_resources;
         };
 
-        template<typename ResourceType, typename... Args>
-        ResourceType* RefCountedResourceManager::GetRawPointer(std::string name, Args&&... arguments)
-        {
-            if (name.empty())
-                return nullptr;
-
-            //Create a typed name so two different types can use the same name
-            name += std::to_string(typeid(ResourceType).hash_code());
-
-            RefCountedResourceManager& _instance = RefCountedResourceManager::GetInstance();
-
-            std::map<std::string, void*>::iterator it = _instance.m_resources.find(name);
-            if (it == _instance.m_resources.end())
-            {
-                //resource not found.  Must allocate
-                ResourceType* resource = new ResourceType(name);
-                if (!resource->Initialize(std::forward<Args>(arguments)...))
-                {
-                    delete resource;
-                    return nullptr;
-                }
-                _instance.m_resources.insert(std::make_pair(name, resource));
-            }
-
-            return reinterpret_cast<ResourceType*>(_instance.m_resources[name]);
-        }
-
-        template<typename ResourceType>
-        void RefCountedResourceManager::ReleaseRawPointer(const std::string& name)
-        {
-            RefCountedResourceManager& _instance = RefCountedResourceManager::GetInstance();
-
-            std::map<std::string, void*>::iterator it = _instance.m_resources.find(name);
-            if (it != _instance.m_resources.end())
-            {
-                delete (reinterpret_cast<ResourceType*>(_instance.m_resources[name]));
-
-                _instance.m_resources.erase(it);
-            }
-        }
+        
     }
 }
---
+++ source/inline/ht_refcounted_resourcemanager.inl
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <ht_refcounted_resourcemanager.h>
+
+namespace Hatchit
+{
+    namespace Core
+    {
+        RefCountedResourceManager::RefCountedResourceManager() : m_resources() {}
+        RefCountedResourceManager::~RefCountedResourceManager()
+        {
+            assert(m_resources.size() == 0);
+        }
+
+        template<typename ResourceType, typename... Args>
+        ResourceType* RefCountedResourceManager::GetRawPointer(const Guid& ID, Args&&... arguments)
+        {
+            if (ID == Guid::Empty)
+                return nullptr;
+
+            //Create a typed name so two different types can use the same name
+            //name += std::to_string(typeid(ResourceType).hash_code());
+
+            RefCountedResourceManager& _instance = RefCountedResourceManager::GetInstance();
+
+            std::map<Guid, void*>::iterator it = _instance.m_resources.find(ID);
+            if (it == _instance.m_resources.end())
+            {
+                //resource not found.  Must allocate
+                ResourceType* resource = new ResourceType(name);
+                if (!resource->Initialize(std::forward<Args>(arguments)...))
+                {
+                    delete resource;
+                    return nullptr;
+                }
+                _instance.m_resources.insert(std::make_pair(ID, resource));
+            }
+
+            return reinterpret_cast<ResourceType*>(_instance.m_resources[ID]);
+        }
+
+        template<typename ResourceType>
+        void RefCountedResourceManager::ReleaseRawPointer(const Guid& ID)
+        {
+            RefCountedResourceManager& _instance = RefCountedResourceManager::GetInstance();
+
+            std::map<Guid, void*>::iterator it = _instance.m_resources.find(ID);
+            if (it != _instance.m_resources.end())
+            {
+                delete (reinterpret_cast<ResourceType*>(_instance.m_resources[ID]));
+
+                _instance.m_resources.erase(it);
+            }
+        }
+    }
+}
