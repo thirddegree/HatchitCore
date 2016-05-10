b3c03c1100579af200bfb44a8ca606ba20978679
Kyle Larson <klarson1331@gmail.com>
2016-05-10 09:59:09 -0400

    Rearranged files to implement new structure for timer, pattern may be re-used for other platform-specific types.

---
+++ include/ht_timer.h
@@ -0,0 +1,65 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2016 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
+
+#pragma once
+
+#include <ht_platform.h> //HT_API
+
+namespace Hatchit
+{
+    namespace Core
+    {
+        /**
+        \interface Hatchit::Core::ITimer
+        \ingroup HatchitCore
+        \brief Interface to describe timer functionality
+
+        Interface that describes the functionality of a timer.
+        Used to link usability between a Windows timer and a
+        Linux timer.
+        **/
+        class HT_API ITimer
+        {
+        protected:
+            ITimer() = default;
+            virtual ~ITimer() = default;
+
+        public:
+            virtual void Start() = 0;
+            virtual void Tick() = 0;
+            virtual void Stop() = 0;
+            virtual void Reset() = 0;
+            virtual float TotalTime() const = 0;
+            virtual float DeltaTime() const = 0;
+        };
+    }    
+}
+
+#if defined(HT_SYS_LINUX)
+#include <ht_linuxtimer.h>
+#else if defined(HT_SYS_WINDOWS)
+#include <ht_windowstimer.h>
+#endif
+
+namespace Hatchit
+{
+    namespace Core
+    {
+        #if defined(HT_SYS_LINUX)
+        using Timer = Linux::Timer;
+        #else if defined(HT_SYS_WINDOWS)
+        using Timer = Windows::Timer;
+        #endif
+    }
+}
---
+++ source/linux/ht_linuxtimer.cpp
@@ -0,0 +1,157 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2016 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
+
+#include <ht_timer.h>
+//We need to include this first, so that there isn't a circular dependency with
+//the aliases used.
+
+namespace Hatchit
+{
+    namespace Core
+    {
+        namespace Linux
+        {
+            /**
+            \fn void Hatchit::Core::Linux::Timer::Timer()
+            \brief Creates instance of timer, ready to be started
+            **/
+            Timer::Timer()
+                : ITimer(),
+                m_previous(),
+                m_totalTime(),
+                m_stopped(true),
+                m_deltaTime(0.0f)
+            {
+            }
+            
+            /**
+            \fn void Hatchit::Core::Linux::Timer::Start()
+            \brief Starts tracking time from moment this function is called.
+
+            Starts tracking time from moment this function is called.
+            **/
+            void Timer::Start()
+            {
+                if (m_stopped)
+                {
+                    clock_gettime(CLOCK_MONOTONIC_RAW, &m_previous);
+                    m_stopped = false;
+                }
+            }
+            
+            /**
+            \fn void Hatchit::Core::Linux::Timer::Stop()
+            \brief Stops tracking timing information
+            **/
+            void Timer::Stop()
+            {
+                m_stopped = true;
+            }
+
+            /**
+            \fn void Hatchit::Core::Linux::Timer::Reset()
+            \brief Resets the timer data.  If timer is currently running, delta
+            time will be calculated between the tick this function is called
+            and the tick that Tick() is called.
+            **/
+            void Timer::Reset()
+            {
+                m_totalTime = timespec();
+                clock_gettime(CLOCK_MONOTONIC_RAW, &m_previous);
+            }
+
+            /**
+            \fn void Hatchit::Core::Linux::Timer::Tick()
+            \brief Recalculates the delta time and total time
+
+            Recalculates the delta time between this tick and the last tick,
+            or the last reset, depending on which happened more recently.
+            Also recalculates the total time by accruing the calculated delta
+            time into the total time.
+            **/
+            void Timer::Tick()
+            {
+                //If timer is stopped, we don't do anything
+                if(m_stopped) {
+                    m_deltaTime = 0.0f;
+                    return;
+                }
+
+                //Query the current time
+                timespec current;
+                clock_gettime(CLOCK_MONOTONIC_RAW, &current);
+                
+                timespec deltaTime;
+                
+                //timespec subtraction
+                if ((current.tv_sec < m_previous.tv_sec) ||
+                    ((current.tv_sec == m_previous.tv_sec) &&
+                    (current.tv_nsec <= m_previous.tv_nsec)))
+                {
+                    deltaTime.tv_sec = deltaTime.tv_nsec = 0;
+                }
+                else
+                {
+                    deltaTime.tv_sec = current.tv_sec - m_previous.tv_sec;
+                    if (current.tv_nsec < m_previous.tv_nsec)
+                    {
+                        deltaTime.tv_nsec = current.tv_nsec + 1000000000L - m_previous.tv_nsec;
+                        deltaTime.tv_sec--;
+                    }
+                    else
+                    {
+                        deltaTime.tv_nsec = current.tv_nsec - m_previous.tv_nsec;
+                    }
+                }
+                
+                //Conversion to float
+                m_deltaTime = static_cast<float>(
+                    static_cast<double>(deltaTime.tv_sec) + 
+                    static_cast<double>(deltaTime.tv_nsec) / 1000000000.0);
+                    
+                //timespec addition
+                m_totalTime.tv_sec += deltaTime.tv_sec;
+                m_totalTime.tv_nsec += deltaTime.tv_nsec;
+                if (m_totalTime.tv_nsec >= 1000000000L)
+                {
+                    m_totalTime.tv_sec++;
+                    m_totalTime.tv_nsec -= 1000000000L;
+                }
+                
+                //Finally, set previous to current time.
+                m_previous = current;
+            }
+
+            /**
+            \fn float Hatchit::Core::Linux::Timer::TotalTime() const
+            \brief Gets total time (in seconds) between when timer was started and last tick.
+            **/
+            float Timer::TotalTime() const
+            {
+                return static_cast<float>(
+                    static_cast<double>(m_totalTime.tv_sec) +
+                    static_cast<double>(m_totalTime.tv_nsec) / 1000000000.0);
+            }
+
+            /**
+            \fn float Hatchit::Core::Linux::Timer::DeltaTime() const
+            \brief Gets time (in seconds) between last two ticks of timer.
+            **/
+            float Timer::DeltaTime() const
+            {
+                return m_deltaTime;
+            }
+        }
+    }
+}
